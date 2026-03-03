8583c691636cbce910b1f7627d9d29456878a35d
yji35j42012 <yji35j42012@gmail.com>
2026-03-03 13:44:29 +0800

    檔案同步

---
+++ ibo/tpl/json/zh-tw/tt_today_game_more.js
@@ -0,0 +1,22 @@
+var artjson = {
+	"ART_lang": "TW",
+	"ART_game_nodata": "目前沒有任何賽事。",
+
+//玩法
+	"ART_game_m": "獨贏",
+	"ART_game_r": "讓局",
+	"ART_game_ou": "總局數: 大 / 小",
+	"ART_game_ouhc": "球員局數:",
+	"ART_game_eo": "總局數: 單 / 雙",
+	"ART_game_pd5": "波膽(5局)",
+	"ART_game_pd7": "波膽(7局)",
+	
+	"ART_game_o_u": "- 大 / 小",
+
+//odd box
+	"ART_game_over": "大",
+	"ART_game_under": "小",
+	"ART_game_odd": "單",
+	"ART_game_even": "雙",
+
+}
